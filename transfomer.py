9289e10f174b9d66fe72667459f1790489a850ba
Shahil <shahilshrestha982@gmail.com>
2025-12-24 22:24:39 +0545

    fished main code

--- transfomer.py
+++ transfomer.py
@@ -60,7 +60,7 @@ class PositionWiseFF(nn.Module):
         super(PositionWiseFF,self).__init__()
         self.fc1=nn.Linear(d_model,d_ff)
         self.fc2=nn.Linear(d_ff,d_model)
-        self.relu=nn.Relu()
+        self.relu=nn.ReLU()
 
     def forward(self,x):
         return self.fc2(self.relu(self.fc1(x)))
@@ -82,12 +82,12 @@ class PositionalEncoding(nn.Module):
         return x+self.pe[:,:x.size(1)]
     
 class EncoderLayer(nn.Module):
-    def __init(self,d_model,num_heads,d_ff,dropout):
+    def __init__(self,d_model,num_heads,d_ff,dropout):
         super(EncoderLayer,self).__init__()
 
         self.self_attn=MultiheadAttention(d_model,num_heads)
         self.ff=PositionWiseFF(d_model,d_ff)
-        self.nomr1=nn.LayerNorm(d_model)
+        self.norm1=nn.LayerNorm(d_model)
         self.norm2=nn.LayerNorm(d_model)
         self.dropout=nn.Dropout(dropout)
 
@@ -96,11 +96,78 @@ class EncoderLayer(nn.Module):
         x=self.norm1(x+self.droput(attn_output))
         ff_output=self.ff(x)
         x=self.norm2(x+self.droput(ff_output))
-        return x
+        return x  #(batch,seq_len,d_model)
     
 
-class Decoder(nn.Module):
-    def __init__(self,d_model,num_heads,d_ff,droput):
+class DecoderLayer(nn.Module):
+    def __init__(self,d_model,num_heads,d_ff,dropout):
         super(DecoderLayer,self).__init__()
 
-        self.self_attn=MultiheadAttention(d_model,)
+        self.self_attn=MultiheadAttention(d_model,num_heads)
+        self.cross_attn=MultiheadAttention(d_model,num_heads)
+        self.ff=PositionWiseFF(d_model,d_ff)
+        self.norm1=nn.LayerNorm(d_model)
+        self.norm2=nn.LayerNorm(d_model)
+        self.norm3=nn.LayerNorm(d_model)
+        self.dropout=nn.Dropout(dropout)
+
+    def forward(self,x,enc_output,src_mask,tgt_mask):
+        attn_output=self.self_attn(x,x,x,tgt_mask)
+        x=self.norm1(x+self.droput(attn_output))
+        attn_output=self.cross_attn(x,enc_output,enc_output,src_mask)
+        x=self.norm2(x+self.droput(attn_output))
+        ff_output=self.ff(x)
+        x=self.norm3(x+self.dropout(ff_output))
+        return x
+    
+class Transformer(nn.Module):
+    def __init__(self,src_vocab_size,tgt_vocab_size,d_model,num_heads,num_layers,d_ff,max_seq_length,dropout):
+        super(Transformer,self).__init__()
+
+        self.encoder_embedding=nn.Embedding(src_vocab_size,d_model)
+        self.decoder_embedding=nn.Embedding(tgt_vocab_size,d_model)
+        self.positional_encoding=PositionalEncoding(d_model,max_seq_length)
+
+        self.encoder_layer=nn.ModuleList([EncoderLayer(d_model,num_heads,d_ff,dropout)for _ in range(num_layers)])
+        self.decoder_layer=nn.ModuleList([DecoderLayer(d_model,num_heads,d_ff,dropout)for _ in range(num_layers)])
+
+        self.fc=nn.Linear(d_model,tgt_vocab_size)
+        self.dropout=nn.Dropout(dropout)
+         
+    def generate_mask(self,src,tgt):
+        src_mask=(src!=0).unsqueeze(1).unsqueeze(2)
+        tgt_mask=(tgt!=0).unsqueeze(1).unsqueeze(3)
+
+        seq_length=tgt.size(1)
+
+        nopeak_mask=(1-torch.triu(torch.ones(1,seq_length,seq_length),diagonal=1)).bool()
+        tgt_mask=tgt_mask& nopeak_mask
+        return src_mask,tgt_mask
+    
+    def forward(self,src,tgt):
+        src_mask,tgt_mask=self.generate_mask(src,tgt)
+        src_embedded=self.dropout(self.positional_encoding(self.encoder_embedding(src)))
+        tgt_embedded=self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
+
+        enc_output=src_embedded
+        for enc_layer in self.encoder_layer:
+            enc_output=enc_layer(enc_output,src_mask)
+
+        dec_output=tgt_embedded
+        for dec_layer in self.decoder_layer:
+            dec_output=dec_layer(dec_output,enc_output,src_mask,tgt_mask)
+
+        output=self.fc(dec_output)
+        return output
+    
+src_vocab_size=5000
+tgt_vocab_size=5000
+d_model=512
+num_heads=8
+num_layers=6
+d_ff=2048
+max_seq_length=100
+dropout=0.1
+transformer=Transformer(src_vocab_size,tgt_vocab_size,d_model,num_heads,num_layers,d_ff,max_seq_length,dropout)
+
+print(transformer)
